43deaf281f310ad0b3ff8942dafe23baef8d20a2
gar-deploy <136169661+gar-deploy@users.noreply.github.com>
2024-03-30 22:00:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use("/dream", dreamRouter);
+app.use("/", dreamRouter);
 app.use("/*", (req, res) => {
   res.status(400).json({Error: "Oopss! Seems link entered wrong URL. Please check the URL again."})
 })
